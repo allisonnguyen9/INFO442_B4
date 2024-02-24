fbf88c7e5d847dd790ec511ae8d2c340e6101381
phan206 <phan206@uw.edu>
2024-02-23 19:48:41 -0800

    added filter some bugs, will fix tomorrow

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
-import { Footer } from './footer.js';
 import  ItemList  from './foodPost.js';
+import {Filter} from './filter.js';
+import { getDatabase, ref as dbRef, set as firebaseSet } from 'firebase/database'
 
 
 function Homepage (props) {
+    const [items, setItems] = useState(props.foodData);
+    const [selectedCategory, setselectedCategory] = useState('');
+
+    function filterPostings(selected) {
+      setselectedCategory(selected);
+    }
+  
+  
+    function clearFilter() {
+      setselectedCategory('');
+    }
+
+    const filteredItems = Array.isArray(props.foodData) ? props.foodData.filter((item) => {
+        const conditionMatch = !selectedCategory || selectedCategory === item.category;
+  
+        return conditionMatch;
+      })
+    : [];
 
   return (
     <div>
       <nav></nav>
       <main>
+        <div className="text-align">
+              <Filter onFilterChange={filterPostings} clearFilter={clearFilter}></Filter>
+        </div>
         <div className='container'>
           <div className='row'>
             {/* <ItemList snackSwap={props.snackSwap}></ItemList> FOR DUMMY DATA*/}
-            <ItemList />
+            <ItemList foodData={filteredItems}></ItemList>
           </div>
         </div>
         <div className="btn-container">
